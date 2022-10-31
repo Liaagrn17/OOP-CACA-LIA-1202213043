1c71a6d01e5d4d50cc173684c4450c53ea1f7c78
Liaagrn17 <91680775+Liaagrn17@users.noreply.github.com>
2022-10-31 08:37:07 +0700

    Add files via upload

---
+++ oop1.java
@@ -0,0 +1,24 @@
+system.out.println("Selamat Datang di Restoran EAD Silahkan Register Terlebih Dahulu" );
+
+public class oop1 {
+
+    public String insertmenu;
+    public String showmenu;
+    public String searchmenu;
+
+   String insert_menu;
+   String show_menu;
+   String search_menu;
+
+   public void setInsertMenu (String insert_menu) {
+    this.insert_menu = insert_menu;
+   }
+
+   public void setShowMenu (String show_menu) {
+    this.show_menu = show_menu;
+   }
+
+   public void setSearchmenu (String search_menu) {
+    this.search_menu = search_menu;
+   }
+}
